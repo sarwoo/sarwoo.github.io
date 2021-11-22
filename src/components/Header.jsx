ca7e0afbc2ee1d60b9021cc9c83edcb5c76beeab
Sarah Wood <spiral464@gmail.com>
2021-11-22 16:57:18 +0000

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-            <h1 className="header-title">HEAD:dev-site | sarwoo</h1>
+            <h1 className="header-title">HEAD:dev-site by sarwoo</h1>
         </header>
 
     )
